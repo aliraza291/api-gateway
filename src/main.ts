0601bf895a0cbe8224fb72b4b62bcc386850a03f
aliraza291 <alirazajutt5412@gmail.com>
2025-07-22 04:06:22 -0700

    minor fix

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-
+import { config } from 'dotenv';
+config(); 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
