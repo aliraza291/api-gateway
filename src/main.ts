ccd068f045ae0c357b1de3463b5e38a42ec5f3a0
aliraza291 <alirazajutt5412@gmail.com>
2025-07-22 23:23:35 -0700

    fix v2

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,26 @@
 import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const PORT = process.env.PORT ?? 3000;
   const app = await NestFactory.create(AppModule);
+
+  // Swagger configuration
+  const config = new DocumentBuilder()
+    .setTitle('API Gateway')
+    .setDescription('API Gate way')
+    .setVersion('1.0')
+    .addTag('api')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
+    console.log(`Swagger documentation available at http://localhost:${PORT}/api`);
   });
 }
 bootstrap();
